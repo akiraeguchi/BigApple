33e8e980d04634d8115cfdb6948431f1b3f8f5b1
akiraeguchi <oneok.enjoymylife@gmail.com>
2021-11-10 15:44:50 +0000

    developing view

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,9 +1,31 @@
 class VideosController < ApplicationController
 
   def index
-    @video = Video.new
+    @video = Video.new(video_params)
+    @videos = Video.all
   end
 
   def show
+    @video = Video.find(params[:id])
   end
+
+  def create
+    @video = Video.new(video_params)
+    url = params[:video][:youtube]
+    url = url.last(11)
+    @video.youtube = url
+    if @video.save
+      redirect_to video_path(@video.id), notice: "動画が投稿されました"
+    else
+      @videos = Video.all
+      @video = video.new(video_params)
+      render :index
+    end
+  end
+
+  private
+  def video_params
+    params.permit(:title, :body, :youtube)
+  end
+
 end
