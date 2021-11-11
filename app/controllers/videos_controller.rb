9bb041965426ee125e241179e0502953db8b5e84
akiraeguchi <oneok.enjoymylife@gmail.com>
2021-11-11 13:51:45 +0000

    fixing view file

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,7 +1,7 @@
 class VideosController < ApplicationController
 
   def index
-    @video = Video.new(video_params)
+    @video = Video.new
     @videos = Video.all
   end
 
@@ -15,17 +15,37 @@ class VideosController < ApplicationController
     url = url.last(11)
     @video.youtube = url
     if @video.save
-      redirect_to video_path(@video.id), notice: "動画が投稿されました"
+      redirect_to videos_path, notice: "動画が投稿されました"
     else
       @videos = Video.all
-      @video = video.new(video_params)
+      @video = video.new
       render :index
     end
   end
 
+  def edit
+    @video = Video.find(params[:id])
+  end
+
+  def update
+    @video = Video.find(params[:id])
+    @video.update(video_params)
+    if @video.save
+      redirect_to videos_path, notice: "動画が更新されました"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @video = Video.find(params[:id])
+    @video.destroy
+    redirect_to videos_path, notice: "動画が削除されました"
+  end
+
   private
   def video_params
-    params.permit(:title, :body, :youtube)
+    params.require(:video).permit(:title, :body, :youtube)
   end
 
 end
